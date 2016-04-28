4d22f11adda0133b7d52ac8e2acf95ca343f543a
Per Andre Stromhaug <pas282@cornell.edu>
2016-04-28 00:58:51 -0400

    made mnist file simple for degbugging

--- test_mnist.cpp
+++ test_mnist.cpp
@@ -15,30 +15,31 @@ using namespace std;
 
 int main(int argc, char** argv)
 {
-	LOG(INFO) << argv[0] << " [GPU] [Device ID]";
-	
-    //Setting CPU or GPU
-	if (argc >= 2 && strcmp(argv[1], "GPU") == 0)
-	{
-		Caffe::set_mode(Caffe::GPU);
-		int device_id = 0;
-		if (argc == 3)
-        {
-      		device_id = atoi(argv[2]);
-    	}
-		Caffe::SetDevice(device_id);
-   		LOG(INFO) << "Using GPU #" << device_id;
-	}
-	else
-	{
-    	LOG(INFO) << "Using CPU";
-    	Caffe::set_mode(Caffe::CPU);
-	}
-	
-	// Set to TEST Phase
-	Caffe::set_phase(Caffe::TEST);
-	
-	// Load net
-	// Assume you are in Caffe master directory
-	Net<float> net("./examples/prediction_example/prediction_example.prototxt");
+	std::cout << "it's working \n";
+//	LOG(INFO) << argv[0] << " [GPU] [Device ID]";
+//	
+//  //SEtting CPU or GPU
+//	if (argc >= 2 && strcmp(argv[1], "GPU") == 0)
+//	{
+//		Caffe::set_mode(Caffe::GPU);
+//		int device_id = 0;
+//		if (argc == 3)
+//        {
+//      		device_id = atoi(argv[2]);
+//    	}
+//		Caffe::SetDevice(device_id);
+//   		LOG(INFO) << "Using GPU #" << device_id;
+//	}
+//	else
+//	{
+//    	LOG(INFO) << "Using CPU";
+//    	Caffe::set_mode(Caffe::CPU);
+//	}
+//	
+//	// Set to TEST Phase
+//	Caffe::set_phase(Caffe::TEST);
+//	
+//	// Load net
+//	// Assume you are in Caffe master directory
+//	Net<float> net("./examples/prediction_example/prediction_example.prototxt");
 }
