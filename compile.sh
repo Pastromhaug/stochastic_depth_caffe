43d2e7a26e580a3d3c3fd7aa41196972cbe9aa05
Per Andre Stromhaug <pas282@cornell.edu>
2016-04-28 01:20:32 -0400

    IT WORKS. pushed -lboost-system flag to the end

--- compile.sh
+++ compile.sh
@@ -1,1 +1,1 @@
-g++ -I  /home/zl499/caffe/include -I /usr/local/cuda-7.5/include -I /home/pas282/stochastic_depth_caffe -lprotobuf test_mnist.cpp
+g++ -I  /home/zl499/caffe/include -I /usr/local/cuda-7.5/include -I /home/pas282/stochastic_depth_caffe -lprotobuf test_mnist.cpp -lboost_system
