2692c1cce2290dcccd9846bcae53b4a8018e4269
Per Andre Stromhaug <pas282@cornell.edu>
2016-04-29 10:34:47 -0400

    simple working solver

--- demonstration_net.cpp
+++ demonstration_net.cpp
@@ -8,6 +8,7 @@
 #include "caffe/caffe.hpp"
 #include "caffe/util/io.hpp"
 #include "caffe/blob.hpp"
+#include "caffe/sgd_solvers.hpp"
 
 using namespace caffe;
 using namespace std;
@@ -17,4 +18,15 @@ int main(int argc, char** argv)
 {
 	std::cout << "it's working\n";
 	Caffe::set_mode(Caffe::GPU);
+	
+	string param_file = "examples/mnist/lenet_solver.prototxt";
+	
+  	SolverParameter param;
+  	ReadSolverParamsFromTextFileOrDie(param_file, &param);
+  	Solver<float>* solver = SolverRegistry<float>::CreateSolver(param);	
+	
+	cout << "solver complete \n";
+
+	solver->Solve();
+	cout << "solved \n";
 }
